9caf91896e1a41d67f09824be8a44a90b7808c08
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-05-03 18:39:47 +0530

    Roboshop Project

--- vm-module/main.tf
+++ vm-module/main.tf
@@ -2,8 +2,8 @@ resource "azurerm_public_ip" "main" {
   name                = "${var.component}-ip"
   resource_group_name = data.azurerm_resource_group.main.name
   location            = data.azurerm_resource_group.main.location
-  allocation_method   = "Dynamic"
-  sku                 = "Basic"
+  allocation_method   = "Static"
+  sku                 = "Standard"
 
   tags = {
     environment ="${var.component}-ip"
