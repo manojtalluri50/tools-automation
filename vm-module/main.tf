d368febb9ff26f8796f15497c402a9d0c88d7f8b
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-05-03 18:58:56 +0530

    Roboshop Project

--- vm-module/main.tf
+++ vm-module/main.tf
@@ -35,7 +35,7 @@ resource "azurerm_network_security_group" "main" {
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
-    source_port_range          = "22"
+    source_port_range          = "*"
     destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
@@ -47,7 +47,7 @@ resource "azurerm_network_security_group" "main" {
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
-    source_port_range          = var.port
+    source_port_range          = "*"
     destination_port_range     = var.port
     source_address_prefix      = "*"
     destination_address_prefix = "*"
