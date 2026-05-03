4ec854d0ca5deec74b80d0434534f404d2b28c5b
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-05-03 18:56:16 +0530

    Roboshop Project

--- vm-module/main.tf
+++ vm-module/main.tf
@@ -63,16 +63,24 @@ resource "azurerm_network_interface_security_group_association" "main" {
   network_security_group_id = azurerm_network_security_group.main.id
 }
 
-resource "azurerm_dns_a_record" "main" {
-  name                = var.component
+resource "azurerm_dns_a_record" "private" {
+  name                = "${var.component}-internal"
   zone_name           = "azdevopsb82.online"
   resource_group_name = data.azurerm_resource_group.main.name
   ttl                 = 10
   records             = [azurerm_network_interface.main.private_ip_address]
 }
 
+resource "azurerm_dns_a_record" "public" {
+  name                = var.component
+  zone_name           = "azdevopsb82.online"
+  resource_group_name = data.azurerm_resource_group.main.name
+  ttl                 = 10
+  records             = [azurerm_public_ip.main.ip_address]
+}
+
 resource "azurerm_virtual_machine" "main" {
-  depends_on            = [azurerm_network_interface_security_group_association.main,azurerm_dns_a_record.main]
+  depends_on            = [azurerm_network_interface_security_group_association.main,azurerm_dns_a_record.private]
   name                  = var.component
   location              = data.azurerm_resource_group.main.location
   resource_group_name   = data.azurerm_resource_group.main.name
