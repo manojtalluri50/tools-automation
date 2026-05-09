5a14ee556e80d62509117a7784bb9488001994eb
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-05-10 00:09:41 +0530

    Roboshop Project

---
+++ misc/vault-secrets/main.tf
@@ -0,0 +1,14 @@
+resource "vault_mount" "kvv1" {
+  for_each    = var.secrets
+  path        = each.key
+  type        = "kv"
+  options     = { version = "1" }
+  description = each.key
+}
+
+variable "secrets" {
+  default = {
+    infra={}
+    roboshop-dev={}
+  }
+}
---
+++ misc/vault-secrets/provider.tf
@@ -0,0 +1,21 @@
+provider "vault" {
+  address = "http://vault-internal.azdevopsb82.online:8200"
+  token = var.token
+}
+
+variable "token" {}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "project-setup-1"
+    storage_account_name = "d82tfstates"
+    container_name       = "vault-tf-states"
+    key                  = "terraform.tfstate"
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "cc2aa876-d510-47ae-88fd-87389092e715"
+}
