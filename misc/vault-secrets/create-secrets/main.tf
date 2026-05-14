e43016d48af442bd44d1d449e20270652db724e9
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-05-15 01:35:59 +0530

    Roboshop Project

--- misc/vault-secrets/create-secrets/main.tf
+++ misc/vault-secrets/create-secrets/main.tf
@@ -6,7 +6,6 @@ resource "vault_mount" "main" {
 }
 
 resource "vault_kv_secret" "secret" {
-  depends_on = [vault_mount.main]
   for_each = var.secrets
   path = "${var.kv_path}/${each.key}"
   data_json = jsonencode(each.value)
