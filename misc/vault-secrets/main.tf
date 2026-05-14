3a37063b3e04ec4993933452b3d9442f15e0969e
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-05-15 01:39:25 +0530

    Roboshop Project

--- misc/vault-secrets/main.tf
+++ misc/vault-secrets/main.tf
@@ -7,18 +7,21 @@ module "create-secrets" {
 
 variable "secrets" {
   default = {
-    infra= {
-      ssh= {
-        admin_username="roboshop-ansible"
-        admin_password="Devops@123456"
+    infra = {
+      ssh = {
+        admin_username = "roboshop-ansible"
+        admin_password = "Devops@123456"
       }
     }
-    roboshop-dev= {
-      catalogue_endpoint = "http://catalogue-dev.azdevopsb82.online:8080"
-      cart_endpoint      = "http://cart-dev.azdevopsb82.online:8080"
-      user_endpoint      = "http://user-dev.azdevopsb82.online:8080"
-      payment_endpoint   = "http://payment-dev.azdevopsb82.online:8080"
-      shipping_endpoint  = "http://shipping-dev.azdevopsb82.online:8080"
+    roboshop-dev = {
+      frontend = {
+        catalogue_endpoint = "http://catalogue-dev.azdevopsb82.online:8080"
+        cart_endpoint      = "http://cart-dev.azdevopsb82.online:8080"
+        user_endpoint      = "http://user-dev.azdevopsb82.online:8080"
+        payment_endpoint   = "http://payment-dev.azdevopsb82.online:8080"
+        shipping_endpoint  = "http://shipping-dev.azdevopsb82.online:8080"
+
+      }
     }
   }
 }
