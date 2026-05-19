58621f1a38a840a2ebd618f283e58a0a0e6f7558
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-05-19 20:29:56 +0530

    Roboshop Project

--- misc/vault-secrets/main.tf
+++ misc/vault-secrets/main.tf
@@ -36,6 +36,10 @@ variable "secrets" {
       catalogue = {
         MONGO       = "true"
         MONGO_URL   = "mongodb://mongodb-dev.azdevopsb82.online:27017/catalogue"
+        DB_TYPE     = "mongo"
+        APP_GIT_URL = "https://github.com/roboshop-devops-project-v3/catalogue"
+        DB_HOST     = "mongodb-dev.azdevopsb82.online"
+        SCHEMA_FILE = "db/master-data.js"
         }
       user = {
         MONGO     = "true"
